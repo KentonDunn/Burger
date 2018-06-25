ffba77f0d71be0d598b7b89b134f058862bf9937
KentonDunn <kenton.dunn@gmail.com>
2018-06-25 15:58:49 -0400

    figured out the methods and the routes, I think

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,98 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 
 var orm = {
-    selectAll: function (tableName) {
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [tableName], function (err, result) {
-            if (err) throw err;
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
             console.log(result);
+            cb(result);
         });
     },
 
-    insertOne: function (tableName, burgerCol, devouredCol, valOfCol) {
-        var queryString = "INSERT INTO ?? (??,??) VALUES (?, FALSE)";
-        connection.query(queryString, [tableName, burgerCol, devouredCol, valOfCol], function (err, result) {
-            if (err) throw err;
+    insertOne: function (table, cols, vals, cb) {
+        //cols will equal burger_name and devoured
+        //vals will be the object created by add a new burger
+        //var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, [table, burgerNameCol, newBurgerName], function (err, result) {
+            if (err) {
+                throw err;
+            }
             console.log(result);
+            cb(result);
         });
     },
 
-    updateOne: function (valOfCol) {
-        var queryString = "Update burgers SET devoured = true WHERE ??, devoured = false";
-        connection.query(queryString, valOfCol)
+    //my example of objColVals is {burger_name: BBQ burger, devoured: false}
+
+    updateOne: function (table, objColVals, condition, cb) {
+        //var queryString = "Update ?? SET devoured = true WHERE ?? = ?";
+        var queryString = "UPDATE" + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     }
 };
 
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,25 @@
 var orm = require("./config/orm.js")
 
-orm.selectAll("burgers");
+var burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
 
-orm.insertOne("burgers", "burger_name", "devoured", "newBurgerName")
+    insertOne: function (columns, values, cb) {
+        orm.insertOne("burgers", columns, values, function (res) {
+            cb(res);
+        });
+    },
 
-orm.updateOne("burger_name")
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
+
+//go to
